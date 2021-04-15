c262200cd059fc5571289865553e20e44d5660e9
Luke A. Guest <laguest@archeia.com>
2021-04-15 14:04:34 +0100

    Add missing colours package.

---
+++ src/colours/rtch-colours.ads
@@ -0,0 +1,24 @@
+package RTCH.Colours with
+  Pure is
+
+   type Colour is record
+      Red   : Float;
+      Green : Float;
+      Blue  : Float;
+   end record;
+
+   function Make_Colour (Red, Green, Blue : Float) return Colour is
+      (Colour'(Red   => Red,
+               Green => Green,
+               Blue  => Blue));
+
+   function "+" (Left, Right : in Colour) return Colour is
+      (Colour'(Red   => Left.Red   + Right.Red,
+               Green => Left.Green + Right.Green,
+               Blue  => Left.Blue  + Right.Blue));
+
+   function "*" (Left : in Colour; Right : in Float) return Colour is
+      (Colour'(Red   => Left.Red   * Right,
+               Green => Left.Green * Right,
+               Blue  => Left.Blue  * Right));
+end RTCH.Colours;
